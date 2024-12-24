d4818af3b448457b137131ebc5b5eb085903f0e9
Maxime Moreillon <m.moreillon@gmail.com>
2024-12-24 15:45:06 +0900

    index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Hono } from 'hono'
-
+import { Hono } from "hono"
+import middleware from "./auth-middleware"
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(middleware({ jwksUri: "http://localhost:8080" }))
+app.get("/", (c) => {
+  return c.text("Hello Hono!")
 })
 
 export default app
