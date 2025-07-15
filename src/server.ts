f8a74279b58553338e3a67cb6d94b345f7527aef
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-15 09:24:34 +0900

    await nex()

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
+import "dotenv/config";
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import middleware from ".";
 const app = new Hono();
 
-app.use(middleware({ jwksUri: "http://localhost:8080" }));
+const { JWKS_URI = "http://localhost:8080" } = process.env;
+
+app.use(middleware({ jwksUri: JWKS_URI }));
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  // @ts-ignore
+  const user = c.get("user");
+  console.log(user);
+  return c.json("user");
 });
 
 serve(app, (info) => {
